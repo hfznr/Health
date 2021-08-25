aa5cae50856672c79770b02b1df859491a425fe8
hfznr <hafizenursahbudak@gmail.com>
2021-08-25 14:34:04 +0300

    MVC.

--- Health/Objects/User.swift
+++ Health/Objects/User.swift
@@ -6,11 +6,221 @@
 //
 
 import UIKit
+import HealthKit
 
 class User: NSObject {
-    var age : Int = 0
-    var blood : String = ""
-    var sex : String = ""
-    var height : Int = 0
-    var weight : Int = 0
+    private var userAge : String? = ""
+    private var userBlood : String? = ""
+    private var userSex : String? = ""
+    private var userHeight : String? = ""
+    private var userWeight : String? = ""
+    
+    let healthKitStore: HKHealthStore = HKHealthStore()
+    let bodyMassType = HKSampleType.quantityType(forIdentifier: .bodyMass)!
+    let heightType = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!
+    
+    func authorizeHealthKitinApp(){
+        let healhKitTypesToRead : Set <HKObjectType> = [HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.biologicalSex)!,HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)!,
+                                                        HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!,
+                                                        HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!,
+                                                        HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!,]
+        let healthKitTypesToWrite : Set <HKSampleType> = [
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!,HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!]
+        
+        if !HKHealthStore.isHealthDataAvailable(){
+            print("Error occured")
+            authorizeHealthKitinApp()
+            return
+        }
+        healthKitStore.requestAuthorization(toShare: healthKitTypesToWrite, read: healhKitTypesToRead) { (success,error) in
+            if let error = error {
+                print("requestAuthorization error:", error.localizedDescription)
+                self.authorizeHealthKitinApp()
+            }
+            
+            if success {
+                print("HealthKit authorization request was successful!")
+                self.readProfile()
+            } else {
+                print("HealthKit authorization was not successful.")
+            }
+            
+        }
+    }
+    
+    
+    func readProfile(){
+        self.userAge = readAge()
+        self.userSex = readSex()
+        self.userBlood = readBlood()
+        readWeight()
+        readHeight()
+    }
+    
+    func readAge()->String?{
+        var age:Int?
+        do{
+            let birthday = try healthKitStore.dateOfBirthComponents()
+            let calendar = Calendar.current
+            let currentyear = calendar.component(.year, from: Date())
+            age = currentyear-birthday.year!
+            let ages = "\((age ?? nil)!)"
+            return ages
+        }catch{
+            return ""
+        }
+       
+    }
+    
+    
+    
+    func readSex()->String?{
+        var gender : String = ""
+        do {
+            let biologicalSex = try healthKitStore.biologicalSex() // try ?
+            switch biologicalSex.biologicalSex.rawValue{
+            case 1:gender = "female"
+            case 2:gender = "male"
+            case 3:gender = "other"
+            default:
+                gender =  ""
+                }
+            }
+            catch{
+                gender = ""
+                 }
+       return gender
+
+        }
+    
+    func readWeight(){
+        var weight : String = ""
+        let query = HKSampleQuery(sampleType: bodyMassType, predicate: nil, limit: HKObjectQueryNoLimit, sortDescriptors: nil)
+        { (query, results, error) in
+            if let result = results?.last as? HKQuantitySample {
+                weight = "\(result.quantity.doubleValue(for: HKUnit.gramUnit(with:.kilo)))"
+                self.userWeight = weight
+            }
+        }
+        healthKitStore.execute(query)
+        
+    }
+    
+    func readHeight(){
+        // tek fonk indirge
+        var height : String = ""
+        let query = HKSampleQuery(sampleType: heightType, predicate: nil, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, results, error) in
+            if let result = results?.last as? HKQuantitySample{
+                height = " \(result.quantity.doubleValue(for: HKUnit.meter()) )"
+               
+                self.userHeight = height
+            }else{
+                print("OOPS didnt get height \nResults => \(results), error => \(error)")
+            }
+        }
+        self.healthKitStore.execute(query)
+    
+        
+    }
+    
+    func readBlood()->String?{
+        var blood : String = ""
+        do {
+            let bloodT = try healthKitStore.bloodType()
+            print(bloodT.bloodType.rawValue)
+            switch (bloodT.bloodType) {
+            case .aPositive: blood = "A+"
+            case .aNegative: blood = "A-"
+            case .bPositive: blood = "B+"
+            case .bNegative: blood = "B-"
+            case .abPositive: blood = "AB+"
+            case .abNegative: blood = "AB-"
+            case .oPositive: blood = "0+"
+            case .oNegative: blood = "0-"
+            default:
+                blood = ""
+                break
+            }
+        }
+        catch{
+            return ""
+        }
+        
+        return blood
+        
+    }
+    
+   
+    
+    func checkAutorizationStatus(identifier : HKObjectType) -> Bool{
+        
+        let authorizationStatus = healthKitStore.authorizationStatus(for:identifier)
+        if authorizationStatus == .notDetermined {
+            print("Authorization Status not determined!")
+            return false
+        } else if authorizationStatus == .sharingDenied {
+            print( "App doesn't have access to your \(identifier.identifier.description) data. You can enable access in the Settings application.")
+               return false
+        }
+        else if authorizationStatus == .sharingAuthorized{
+            return true
+        }
+        return false
+    }
+    
+    
+    func writeToKit(weightText : String? , heightText : String?){
+        let today = NSDate()
+        if checkAutorizationStatus(identifier: HKSampleType.quantityType(forIdentifier: .bodyMass)!){
+            let weight = Double(weightText ?? "")
+            print ("w:\(weight)")
+            if (weight != nil){
+                if let type = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass){
+                    let quantity = HKQuantity(unit:HKUnit.gram(),doubleValue:Double(weight!))
+                    let sample = HKQuantitySample(type: type , quantity: quantity,start: today as Date,end: today as Date)
+                    healthKitStore.save(sample) { (success, error) in
+                        if success {
+                            self.userWeight = weightText
+                        }
+                        print("Saved \(success),error \(error)")
+                    }
+                }
+                
+            }
+        }
+        
+        if checkAutorizationStatus(identifier: HKSampleType.quantityType(forIdentifier: .height)!){
+            let height = Double(heightText ?? "")
+            print("h:\(heightText)")
+            if (height != nil){
+                if let type = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height) {
+                    let quantity = HKQuantity(unit: HKUnit.inch(), doubleValue: height!)
+                    let sample = HKQuantitySample(type: type, quantity: quantity, start: today as Date, end: today as Date)
+                    
+                    self.healthKitStore.save(sample, withCompletion: { (success, error) in
+                        if success {
+                            self.userHeight = heightText
+                        }
+                    })
+                }
+                
+            }
+        }
+        }
+    
+    func getUserSex()->String?{
+        return self.userSex
+    }
+    func getUserBlood()->String?{
+        return self.userBlood
+    }
+    func getUserAge()->String?{
+        return self.userAge
+    }
+    func getUserWeight()->String?{
+        return self.userWeight
+    }
+    func getUserHeight()->String?{
+        return self.userHeight
+    }
 }
--- Health/ViewControllers/ViewController.swift
+++ Health/ViewControllers/ViewController.swift
@@ -12,219 +12,53 @@ class ViewController: UIViewController {
     
 
     
-    @IBOutlet weak var descriptionLabel: UILabel!
+   
     
     
     var hasRequestedHealthData: Bool = false
     @IBOutlet weak var sexLabel: UILabel?
-    @IBOutlet weak var ageLabel: UILabel!
-    @IBOutlet weak var bloodLable: UILabel!
-    @IBOutlet weak var weightLabel: UILabel!
-    @IBOutlet weak var heightLabel: UILabel!
-    
+    @IBOutlet weak var ageLabel: UILabel?
+    @IBOutlet weak var bloodLable: UILabel?
+    @IBOutlet weak var weightLabel: UILabel?
+    @IBOutlet weak var heightLabel: UILabel?
+    @IBOutlet weak var descriptionLabel: UILabel?
     
     @IBOutlet weak var weightTextField: UITextField!
     @IBOutlet weak var heightTextField: UITextField!
     
     let user = User()
-    let healthKitStore: HKHealthStore = HKHealthStore()
-    let bodyMassType = HKSampleType.quantityType(forIdentifier: .bodyMass)!
-    let heightType = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        authorizeHealthKitinApp()
+        user.authorizeHealthKitinApp()
+        readUserInfo()
         
     }
     @IBAction func saveButtonClicked(_ sender: Any) {
-        writeToKit()
+        
+        user.writeToKit(weightText:self.weightTextField?.text!, heightText: self.heightTextField?.text!)
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func updateButtonClicked(_ sender: UIButton) {
-        self.readProfile()
+       readUserInfo()
     }
     
-    func authorizeHealthKitinApp(){
-        let healhKitTypesToRead : Set <HKObjectType> = [HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.biologicalSex)!,HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)!,
-                                                        HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!,
-                                                        HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!,
-                                                        HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!,]
-        let healthKitTypesToWrite : Set <HKSampleType> = [
-            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!,HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!]
-        
-        if !HKHealthStore.isHealthDataAvailable(){
-            print("Error occured")
-            authorizeHealthKitinApp()
-            return
+    func readUserInfo(){
+        user.readProfile()
+        DispatchQueue.main.async {
+            self.sexLabel?.text = self.user.getUserSex()
+            self.ageLabel?.text = self.user.getUserAge()
+            self.heightLabel?.text = self.user.getUserHeight()
+            self.weightLabel?.text = self.user.getUserWeight()
+            self.bloodLable?.text = self.user.getUserBlood()
         }
-        healthKitStore.requestAuthorization(toShare: healthKitTypesToWrite, read: healhKitTypesToRead) { (success,error) in
-            if let error = error {
-                print("requestAuthorization error:", error.localizedDescription)
-                self.authorizeHealthKitinApp()
-            }
-            
-            if success {
-                print("HealthKit authorization request was successful!")
-                self.readProfile()
-            } else {
-                print("HealthKit authorization was not successful.")
-            }
-            
-        }
-        let authorizationStatusMass = healthKitStore.authorizationStatus(for: HKSampleType.quantityType(forIdentifier: .bodyMass)!)
-        if authorizationStatusMass == .notDetermined {
-            weightLabel.isHidden = true
-        } else if authorizationStatusMass == .sharingDenied {
-            print( "App doesn't have access to your bodymass data. You can enable access in the Settings application.")
-            if self.weightLabel != nil {
-                weightLabel.text = ""
-            }
-            
-        }
-        let authorizationStatusHeight = healthKitStore.authorizationStatus(for: HKSampleType.quantityType(forIdentifier: .height)!)
-        if authorizationStatusHeight == .notDetermined {
-            weightLabel.isHidden = true
-        } else if authorizationStatusHeight == .sharingDenied {
-            print( "App doesn't have access to your Height data. You can enable access in the Settings application.")
-            if self.heightLabel != nil {
-                heightLabel.text = ""
-            }
-            
-        }
-        
     }
     
     
-    func readProfile(){
-        var age:Int?
-        var weight : String = ""
-        var height : String = ""
-        var gender : String = ""
-        var blood : String = ""
-        
-        do{
-            let birthday = try healthKitStore.dateOfBirthComponents()
-            let calendar = Calendar.current
-            let currentyear = calendar.component(.year, from: Date())
-            age = currentyear-birthday.year!
-            let query = HKSampleQuery(sampleType: bodyMassType, predicate: nil, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, results, error) in
-                if let result = results?.last as? HKQuantitySample {
-                    DispatchQueue.main.async {
-                        weight = "\(result.quantity.doubleValue(for: HKUnit.gramUnit(with: .kilo)))"
-                        if self.weightLabel != nil{
-                            self.weightLabel.text = weight
-                        }
-                        let ages = "\((age ?? nil)!)"
-                        if self.ageLabel != nil {
-                            self.ageLabel.text = ages
-                        }
-                    }
-                }
-            }
-            healthKitStore.execute(query)
-        }
-        catch{
-            DispatchQueue.main.async {
-                if self.ageLabel != nil {
-                    self.ageLabel.text = ""
-                }
-            }
-        }
-        
-        do {
-            let biologicalSex = try healthKitStore.biologicalSex() // try ?
-            switch biologicalSex.biologicalSex.rawValue{
-            case 1:gender = "female"
-            case 2:gender = "male"
-            case 3:gender = "other"
-            default:
-                gender =  ""
-            }
-            // tek fonk indirge(generic fonk)
-            let query = HKSampleQuery(sampleType: heightType, predicate: nil, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, results, error) in
-                if let result = results?.last as? HKQuantitySample{
-                    
-                    print("Height => \(result.quantity)")
-                    DispatchQueue.main.async {
-                        if(self.heightLabel != nil){
-                            self.heightLabel.text = " \(result.quantity)"
-                        }
-                    }
-                }else{
-                    print("OOPS didnt get height \nResults => \(results), error => \(error)")
-                }
-            }
-            self.healthKitStore.execute(query)
-            DispatchQueue.main.async {
-                self.sexLabel?.text = gender
-//                if self.sexLabel != nil{
-//                    self.sexLabel.text = gender
-//                }
-                
-            }
-            
-        }
-        catch{}
-        do {
-            let bloodT = try healthKitStore.bloodType()
-            print(bloodT.bloodType.rawValue)
-            switch (bloodT.bloodType) {
-            case .aPositive: blood = "A+"
-            case .aNegative: blood = "A-"
-            case .bPositive: blood = "B+"
-            case .bNegative: blood = "B-"
-            case .abPositive: blood = "AB+"
-            case .abNegative: blood = "AB-"
-            case .oPositive: blood = "0+"
-            case .oNegative: blood = "0-"
-                
-            default:
-                blood = ""
-                break
-            }
-            DispatchQueue.main.async {
-                if self.bloodLable != nil{
-                    self.bloodLable.text = blood
-                }
-                
-                
-            }
-        }
-        catch{}
-        
-    }
     
-    func writeToKit(){
-        let weight = Double(self.weightTextField.text!)
-        let today = NSDate()
-        if (weight != nil){
-            if let type = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass){
-                let quantity = HKQuantity(unit:HKUnit.gram(),doubleValue:Double(weight!))
-                let sample = HKQuantitySample(type: type , quantity: quantity,start: today as Date,end: today as Date)
-                healthKitStore.save(sample) { (success, error) in
-                    print("Saved \(success),error \(error)")
-                }
-            }
-            
-        }
-        
-        
-        let height = Double(self.heightTextField.text!)
-        if (height != nil){
-            if let type = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height) {
-                let quantity = HKQuantity(unit: HKUnit.inch(), doubleValue: height!)
-                let sample = HKQuantitySample(type: type, quantity: quantity, start: today as Date, end: today as Date)
-                
-                self.healthKitStore.save(sample, withCompletion: { (success, error) in
-                    if success {
-                        print("YESSS")
-                    }
-                })
-            }
-            
-        }
-    }
+    
 }
     //-Kullanılmayan kısım
     
